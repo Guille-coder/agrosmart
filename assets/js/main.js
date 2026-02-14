936c7cdb5cf71d1160a9fd1c55fd9eed25e6fe99
Guille-coder <guillermoreyen.16@gmail.com>
2026-02-14 11:36:21 -0500

    new index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 const loginForm = document.getElementById("loginForm");
 
-loginForm.addEventListener("submit", function(e) {
-  e.preventDefault();
+if (loginForm) {
+  loginForm.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-  const email = document.getElementById("email").value;
+    const email = document.getElementById("email").value;
+    localStorage.setItem("userEmail", email);
 
-  localStorage.setItem("userEmail", email);
-
-  window.location.href = "dashboard.html";
-});
+    window.location.href = "dashboard.html";
+  });
+}
